87795ecce6c9899ecf5db7232981c8891eb06715
Bangdayeon <dybang00@gmail.com>
2026-02-02 11:53:06 +0900

    feat: add cardlist (#28)

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -15,19 +15,20 @@ export default function Card({ image, tags, title, uploadDate }: CardProps) {
   const isoDate = uploadDate.toISOString();
 
   return (
-    <div className="border-gray100 flex h-50 w-38 flex-col rounded-2xl border md:h-76 md:w-52">
-      <div className="border-gray100 flex h-40 w-38 items-center justify-center border-b md:h-50 md:w-52">
-        <div className="relative h-30 w-25 md:h-46 md:w-48">
-          <Image src={image} alt="" fill className="object-contain" />
+    <div className="border-gray100 flex w-full flex-col rounded-2xl border">
+      <div className="border-gray100 aspect-18/20 w-full border-b">
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="relative h-[90%] w-[90%]">
+            <Image src={image} alt="" fill className="object-contain" />
+          </div>
         </div>
       </div>
-      <div className="flex h-25 flex-col justify-between px-3.5 py-2">
+      <div className="flex flex-col justify-between gap-2 px-3.5 py-2">
         <div className="flex flex-col gap-1">
-          <div className="flex items-center justify-between">
-            <span className="font-title-sm md:text-[20px]">{title}</span>
-            <div className="flex">
+          <div className="flex items-center justify-between gap-2">
+            <span className="font-title-sm line-clamp-1 md:text-[20px]">{title}</span>
+            <div className="flex shrink-0">
               <IconButton icon="IC_Download" variant="ghost" ariaLabel="파일 다운" />
-              {/* <IconButton icon="IC_Bookmark" variant="ghost" ariaLabel="북마크" /> TODO: 추후 구현*/}
             </div>
           </div>
           <div className="hidden gap-1 md:flex">
@@ -37,7 +38,6 @@ export default function Card({ image, tags, title, uploadDate }: CardProps) {
           </div>
         </div>
         <div className="flex items-center justify-between">
-          {/* <LikeButton /> TODO: 추후 구현*/}
           <span className="font-body-sm text-gray400">
             등록일 | <time dateTime={isoDate}>{formattedDate}</time>
           </span>
