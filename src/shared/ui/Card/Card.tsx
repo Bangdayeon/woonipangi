aaadbd9c4392fa361884674aefed38e0aa6189ee
Bangdayeon <dybang00@gmail.com>
2026-02-10 23:56:10 +0900

    feat: add datas (#54)

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -21,23 +21,19 @@ export default function Card({ id, title, tags, fileUrls, thumbnail, createdAt }
         </div>
       </div>
       <div className="flex flex-col justify-between gap-2 px-3.5 py-2">
-        <div className="flex flex-col gap-1">
-          <div className="flex items-center justify-between gap-2">
-            <span className="font-title-sm line-clamp-1 md:text-[20px]">{title}</span>
-            <div className="flex shrink-0">
-              <IconButton icon="IC_Download" variant="ghost" ariaLabel="파일 다운" />
-            </div>
-          </div>
-          <div className="hidden gap-1 md:flex">
-            {tags.map((tag, index) => (
-              <Tag key={`${tag}-${index}`} text={tag} />
-            ))}
-          </div>
+        <div className="flex items-center justify-between gap-2">
+          <span className="font-title-sm line-clamp-1 md:text-[16px]">{title}</span>
         </div>
-        <div className="flex items-center justify-between">
+        <div className="hidden gap-1 md:flex">
+          {tags.map((tag, index) => (
+            <Tag key={`${tag}-${index}`} text={tag} />
+          ))}
+        </div>
+        <div className="flex items-end justify-between">
           <span className="font-body-sm text-gray400">
-            등록일 | <time dateTime={isoDate}>{formattedDate}</time>
+            제작일 | <time dateTime={isoDate}>{formattedDate}</time>
           </span>
+          <IconButton icon="IC_Download" variant="ghost" ariaLabel="파일 다운" />
         </div>
       </div>
     </div>
--- src/widget/FilesPage/FilesPage.tsx
+++ src/widget/FilesPage/FilesPage.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { CardDatas } from '@/data/cards';
 import CardList from '@/shared/ui/CardList';
 
 export default function FilesPage() {
@@ -19,7 +20,7 @@ export default function FilesPage() {
     <div className="my-8 flex h-full justify-center px-4">
       <div className="w-full md:max-w-200 lg:max-w-300">
         <h1 className="mb-6 text-2xl font-bold md:text-3xl">캐릭터 파일 다운 페이지</h1>
-        <CardList cards={sampleCards} />
+        <CardList cards={CardDatas} />
       </div>
     </div>
   );
