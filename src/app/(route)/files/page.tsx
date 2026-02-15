03bea05458db65c66e44dbed4a2c86c5f213eed4
Bangdayeon <dybang00@gmail.com>
2026-02-15 18:34:38 +0900

    feat: add loading and error page (#74)

--- src/app/(route)/files/page.tsx
+++ src/app/(route)/files/page.tsx
@@ -1,11 +1,5 @@
 import FilesPage from '@/widget/FilesPage/FilesPage';
-import { Suspense } from 'react';
 
 export default function Page() {
-  return (
-    // TODO: 추후 수정
-    <Suspense fallback={<div>Loading...</div>}>
-      <FilesPage />
-    </Suspense>
-  );
+  return <FilesPage />;
 }
