a5a8e4438cec2d4c66eefc37411f6e703ccef4ac
Bangdayeon <dybang00@gmail.com>
2026-04-08 18:51:45 +0900

    refactor: modify pangs conch structure

--- src/widget/GamesPage/components/TestCard.tsx
+++ src/widget/GamesPage/components/TestCard.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 interface Props {
   title: string;
   description: string;
-  href: string;
+  href?: string;
   img?: string | StaticImageData;
   bgcolor: string;
 }
