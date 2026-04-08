7098bb6bff8df44b908066704d815155eb74f736
Bangdayeon <dybang00@gmail.com>
2026-04-08 19:47:30 +0900

    refactor: change conch name

--- src/app/(route)/games/pangs-conch/page.tsx
+++ src/app/(route)/games/pangs-conch/page.tsx
@@ -1,20 +1,20 @@
 import PangsConch from '@/widget/GamesPage/TestPages/PangsConch';
 
 export const metadata = {
-  title: '팡이의 소라고둥',
+  title: '마법의 팡이고둥',
   description: '팡이의 마법의 소라고둥에게 질문해보세요',
   openGraph: {
-    title: '팡이의 소라고둥',
-    description: '팡이의 소라고둥에게 질문해보세요',
+    title: '마법의 팡이고둥',
+    description: '마법의 팡이고둥에게 질문해보세요',
     images: ['/images/services/pang_conch.png'],
   },
   twitter: {
-    title: '팡이의 소라고둥',
-    description: '팡이의 소라고둥에게 질문해보세요',
+    title: '마법의 팡이고둥',
+    description: '마법의 팡이고둥에게 질문해보세요',
     images: ['/images/services/pang_conch.png'],
   },
 };
 
 export default function PangsConchPage() {
-  return <PangsConch description="팡이의 소라고둥에게 질문해보세요" />;
+  return <PangsConch description="마법의 팡이고둥에게 질문해보세요" />;
 }
